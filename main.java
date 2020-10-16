f17d76ac17cb8039a8e67785672ff769adfe3565
cno <co@localhost.localdomain>
2020-10-16 11:22:21 +0200

    Initiales Programm. Die zusätzlichen Dateien werden noch hinzugefügt. Neuere Textdateien müssen in Zukunft verschlüsselt werden.

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class main{
+	public static void main(String[] args){
+		File file = new File("p_teams.ftb");
+		Scanner f_scan = new Scanner(file);
+	}
+}
