2df399613e07d29b09285458aa4624254234bb80
jv <cifer@cifersmethod.com>
2020-10-16 12:45:11 +0200

    - issue corrected - exception added

--- main.java
+++ main.java
@@ -3,7 +3,11 @@ import java.io.*;
 
 public class main{
 	public static void main(String[] args){
-		File file = new File("p_teams.ftb");
-		Scanner f_scan = new Scanner(file);
+		try {
+			File file = new File("p_teams.ftb");
+			Scanner f_scan = new Scanner(file);
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
 	}
 }
